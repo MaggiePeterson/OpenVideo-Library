3a3cf1f532cbabc88aec20d11737c9af10c76045
MaggiePeterson <margaretpeterson@sfhs.com>
2018-11-03 14:43:27 -0700

    Initial Commit

---
+++ cameraSettingsLibUVC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cameraSettingsLibUVC
+//
+//  Created by Margaret Peterson on 11/3/18.
+//  Copyright © 2018 Margaret Peterson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
