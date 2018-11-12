2de11510d5546163e5ec2ee813175599c6d7971d
MaggiePeterson <margaretpeterson@sfhs.com>
2018-11-11 21:48:34 -0800

    first commit

---
+++ cameraSettingsLibUVC/OpenVideo.cpp
@@ -0,0 +1,9 @@
+//
+//  OpenVideo.cpp
+//  cameraSettingsLibUVC
+//
+//  Created by Margaret Peterson on 11/10/18.
+//  Copyright © 2018 Margaret Peterson. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ cameraSettingsLibUVC/OpenVideo.h
@@ -0,0 +1,13 @@
+//
+//  OpenVideo.h
+//  cameraSettingsLibUVC
+//
+//  Created by Margaret Peterson on 11/10/18.
+//  Copyright © 2018 Margaret Peterson. All rights reserved.
+//
+
+#ifndef OpenVideo_h
+#define OpenVideo_h
+
+
+#endif /* OpenVideo_h */
