533867fa3cb154b0a25c42daeeac02421d5b601c
MaggiePeterson <32180050+MaggiePeterson@users.noreply.github.com>
2018-11-17 09:45:22 -0800

    can see frame

--- cameraSettingsLibUVC/main.cpp
+++ cameraSettingsLibUVC/main.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  cameraSettingsLibUVC
@@ -7,9 +8,35 @@
 //
 
 #include <iostream>
+#include"libuvc.h"
+#include <stdio.h>
+#include <unistd.h>
+#include "OpenVideo.h"
+using namespace cv;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+/* This callback function runs once per frame. Use it to perform any
+ * quick processing you need, or have it put the frame into your application's
+ * input queue. If this function takes too long, you'll start losing frames. */
+
+int main(int argc, char **argv) {
+    
+   /* SAMPLE CODE
+    // opens camera and returns one frame
+    
+    OpenVideo myCamera(1); //OpenVideo objects takes in int CameraIndex
+    imshow("Frame", myCamera.getImage());
+    waitKey(0);
+    */
+
+    //opens camera continuously until user presses q
+    OpenVideo myCamera(1); //OpenVideo objects takes in int CameraIndex
+    char keyboard = 0;
+    while (keyboard != 'q'){ // press q to stop stream
+        imshow("Frame", myCamera.getImage());
+        keyboard = (char)waitKey( 30 );
+    }
+    cvDestroyWindow("Frame");
+    
     return 0;
 }
