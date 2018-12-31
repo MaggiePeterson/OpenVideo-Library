d7475aaee866f71540212dfa2a80ec62d5309658
MaggiePeterson <32180050+MaggiePeterson@users.noreply.github.com>
2018-12-30 21:29:42 -0800

    Add files via upload

--- installOpenVideo.sh
+++ installOpenVideo.sh
@@ -13,9 +13,11 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 sudo make install
 
-cd..
-cd..
+cd ..
+cd ..
 brew tap homebrew/science
 brew install opencv3
 
 git clone https://github.com/MaggiePeterson/OpenVideo-Library.git
+
+echo "Installation finished. <₍⸍⸌̣ʷ̣̫⸍̣⸌₎/ "
