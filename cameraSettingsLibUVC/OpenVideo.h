0f45b69ea3d31ee930a7927723d770f0e506e43c
MaggiePeterson <32180050+MaggiePeterson@users.noreply.github.com>
2019-01-06 21:15:01 -0800

    updated

--- cameraSettingsLibUVC/OpenVideo.h
+++ cameraSettingsLibUVC/OpenVideo.h
@@ -8,6 +8,43 @@
 
 #ifndef OpenVideo_h
 #define OpenVideo_h
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
 
+
+#include "imgcodecs.hpp"
+#include "imgproc.hpp"
+#include "videoio.hpp"
+#include "highgui.hpp"
+#include "video.hpp"
+
+#include <iostream>
+#include"libuvc.h"
+#include <stdio.h>
+#include <unistd.h>
+
+//C++
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
 
 #endif /* OpenVideo_h */
+
+class OpenVideo {
+    
+public:
+    OpenVideo(int camNum);
+    static void cb(uvc_frame_t *frame, void *ptr);
+    Mat getImage();
+    
+private:
+    void ChangeExposure();
+    int webCamIndex;
+    VideoCapture *capture;
+};
+
+
